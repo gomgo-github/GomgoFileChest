0052a7b891e095c304b81a2df1d697189fe36583
gomgo-github <gomgounity@gmail.com>
2024-01-22 14:07:58 +0100

    Updated "Chinesse iBus Restore.sh" to create a Persistent Metgod

--- evilar/custom-settings/ibus/Chinesse iBus Restore.sh
+++ evilar/custom-settings/ibus/Chinesse iBus Restore.sh
@@ -2,10 +2,14 @@
 # Code and Repo by [+[-GMG-]]
 # Script: Create persistent code to restore previously installed libpinyin on to iBus Settings to avoid Puppet Restore
 
-
+# Download prerequisited libpinyin settings in directory  
 cd /home/usuario/.config/dconf/
 wget https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/custom-settings/ibus/CN_dump.dconf
 bash -c "/usr/bin/dconf load /desktop/ibus/ < /home/usuario/.config/dconf/CH_dump.dconf"
+
+# Download Persistency Method on Autostart Directory
+cd /home/usuario/.config/autostart
+wget https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/custom-settings/ibus/CN_dconf_ibus.desktop
 
 # "It's not 'bout people being 'Xunta' it's being a Community"
 # Fuck <Abalar Regional Accesibility> Script 
