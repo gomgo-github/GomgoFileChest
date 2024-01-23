55da784c10fe8f1719420382cd439080f31a2d86
gomgo-github <gomgounity@gmail.com>
2024-01-23 13:33:53 +0100

    Update MENU.sh

--- evilar/MENU.sh
+++ evilar/MENU.sh
@@ -2,6 +2,7 @@
 # Bash Menu Script for Evilar
 # Thx to #sudoer on StackOverflow for the Menu Template
 
+clear
 echo "================================================="
 echo "                 Evilar Int Menu                 "
 echo "================================================="
