7b8d4fe7a4b2a59e0c80c6bcfc2187347ed0e2c7
gomgo-github <gomgounity@gmail.com>
2024-01-23 12:34:28 +0100

    Update MENU.sh

--- evilar/MENU.sh
+++ evilar/MENU.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 # Bash Menu Script for Evilar
 # Thx to #sudoer on StackOverflow for the Menu Template
+
+echo "================================================="
+echo "                 Evilar Int Menu                 "
+echo "================================================="
+echo ""
+echo " 1) Instalar Persistencia de Teclado Chino"
+echo " "
+echo ""
+echo ""
+echo ""
+echo ""
+echo " 0) Quit"
+echo ""
+echo ""
+
+echo "Escribe el Número del Programa a ejecutar (0-1):"
+read n
+case $n in
+  0) break;;
+  1) sh -c "$(curl -sSL https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/custom-settings/ibus/check.sh)";;
+  *) echo "invalid option";;
+esac
