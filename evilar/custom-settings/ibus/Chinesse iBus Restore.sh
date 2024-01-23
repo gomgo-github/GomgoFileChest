f281cf044360919c7625a1d796c74071d3d400f0
gomgo-github <gomgounity@gmail.com>
2024-01-23 10:08:26 +0100

    Update Chinesse iBus Restore.sh to avoid duplicated files
    Now program works as an Installer/Updater by deleting and redownloading files

--- evilar/custom-settings/ibus/Chinesse iBus Restore.sh
+++ evilar/custom-settings/ibus/Chinesse iBus Restore.sh
@@ -4,14 +4,21 @@
 
 # Download prerequisited libpinyin settings in directory  
 cd /home/usuario/.config/dconf/
-wget https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/custom-settings/ibus/CN_dump.dconf
-bash -c "/usr/bin/dconf load /desktop/ibus/ < /home/usuario/.config/dconf/CH_dump.dconf"
+if [ -f /home/usuario/.config/dconf/CN_dump.dconf ]; then rm /home/usuario/.config/dconf/CN_dump.dconf;
+echo "OK"
+wget https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/custom-settings/ibus/CN_dump.dconf; fi
+bash -c "/usr/bin/dconf load /desktop/ibus/ < /home/usuario/.config/dconf/CN_dump.dconf"
+echo "OK"
 
 # Download Persistency Method on Autostart Directory
 cd /home/usuario/.config/autostart
+if [ -f /home/usuario/.config/autostart/CN_dconf_ibus.desktop ]; then rm /home/usuario/.config/autostart/CN_dconf_ibus.desktop;fi
+echo "OK"
+if [ -f /home/usuario/.config/autostart/CN_Script ]; then rm /home/usuario/.config/autostart/CN_Script; fi
+echo "OK"
 wget https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/custom-settings/ibus/CN_dconf_ibus.desktop
 wget https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/custom-settings/ibus/CN_Script
-chmod a+x /home/usuario/.config/autostart/CN_Script
+echo "OK"
 
 # "It's not 'bout people being 'Xunta' it's being a Community"
 # Fuck <Regional Accesibility> Script 
