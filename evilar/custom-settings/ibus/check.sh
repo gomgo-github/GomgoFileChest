c2eaafa477c630a9d271ede77dc4efbdf7af766b
gomgo-github <gomgounity@gmail.com>
2024-01-25 15:47:34 +0100

    Update check.sh
    Updated to Chinesse

--- evilar/custom-settings/ibus/check.sh
+++ evilar/custom-settings/ibus/check.sh
@@ -6,18 +6,20 @@ clear
 echo "================================================="
 echo "                 Evilar Int Menu                 "
 echo "================================================="
+echo "         检查是否安装了中文字体"
 echo "         Check for Chinesse Font Installed"
 echo " Check if you can read following text:"
+echo "检查您是否能阅读以下文字："
 echo ""
 echo "  如果您能读懂，就可以继续。键入 '0'"
 echo ""
 echo ""
 echo ""
-echo "Continuar?"
-echo " 0) Si"
-echo " 1) No"
+echo "继续？"
+echo " 0) 继续 (Y)"
+echo " 1) 塞拉尔 (N)"
 
-echo "Continuar?:"
+echo "继续？:"
 read n
 case $n in
   0) sh -c "$(curl -sSL https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/custom-settings/ibus/Chinesse%20iBus%20Restore.sh)";;
