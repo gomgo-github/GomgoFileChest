88d4353e9d6f7c98e0151e3b4d756355548daad4
gomgo-github <gomgounity@gmail.com>
2024-01-30 13:56:22 +0100

    Update Desktop Setting Restore.sh

--- evilar/custom-settings/XFCE_WallpaperHotfix/Desktop Setting Restore.sh
+++ evilar/custom-settings/XFCE_WallpaperHotfix/Desktop Setting Restore.sh
@@ -22,7 +22,7 @@ cd /home/usuario/.config/autostart
 if [ -f /home/usuario/.config/autostart/WallpaperHotfix.desktop ]; then rm /home/usuario/.config/autostart/WallpaperHotfix.desktop;fi
 echo "OK"
 
-wget https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/custom-settings/XFCE_WallpaperHotfix/Modules/Patches/WallpaperHotfix.desktop
+wget https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/custom-settings/XFCE_WallpaperHotfix/Modules/Patches/WallpaperHotfix.sh
 echo "OK"
 
 # Give Permissions to downloaded executable files
