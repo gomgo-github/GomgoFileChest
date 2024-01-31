767e46020f7ebed82e0632b4fc68e6eaa59ed2d1
gomgo-github <gomgounity@gmail.com>
2024-01-31 12:48:33 +0100

    Update MENU.sh

--- evilar/Menus/MENU.sh
+++ evilar/Menus/MENU.sh
@@ -8,7 +8,7 @@ echo "================================================="
 echo ""
 echo " 1) Instalar persistencia de Teclado Chino"
 echo "     (安装中文键盘补丁)"
-echo ""
+echo " 2) Instalar parche de Fondos de Pantalla"
 echo ""
 echo ""
 echo ""
@@ -22,5 +22,6 @@ read n
 case $n in
   0) break;;
   1) sh -c "$(curl -sSL https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/custom-settings/ibus/check.sh)";;
+  2) sh -c "$(curl -sSL https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/custom-settings/XFCE_WallpaperPatch/check.sh)";;
   *) echo "invalid option";;
 esac
