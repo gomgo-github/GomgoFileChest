b66529842ba980e9e3b7d0c4f868af3239c65e6e
gomgo-github <gomgounity@gmail.com>
2024-02-07 21:22:57 +0100

    Update CN_Updater.sh

--- Versiones/Updaters/CN_ibus/CN_Updater.sh
+++ Versiones/Updaters/CN_ibus/CN_Updater.sh
@@ -2,4 +2,9 @@
 
 #TODO: Code
 
+#Actualizar Versión
+cd /home/usuario/.config/autostart/Modules/Versions/
+rm -f /home/usuario/.config/autostart/Modules/Versions/CN_ibus.v
+wget https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/Versiones/release/CN_ibus.v
+
 notify-send -t 2000 "Parche de Teclado Chino actualizado!"
