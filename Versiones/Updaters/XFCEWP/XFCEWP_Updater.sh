381b71772b8d1a354d6a4d21285da64b3c36dd9a
gomgo-github <gomgounity@gmail.com>
2024-02-07 21:23:34 +0100

    Update XFCEWP_Updater.sh

--- Versiones/Updaters/XFCEWP/XFCEWP_Updater.sh
+++ Versiones/Updaters/XFCEWP/XFCEWP_Updater.sh
@@ -2,4 +2,9 @@
 
 #TODO: Code
 
+#Actualizar Versión
+cd /home/usuario/.config/autostart/Modules/Versions/
+rm -f /home/usuario/.config/autostart/Modules/Versions/XFCEWP.v
+wget https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/Versiones/release/XFCEWP.v
+
 notify-send -t 2000 "Parche de Fondo de Pantalla actualizado!"
