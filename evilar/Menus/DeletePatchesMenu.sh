33d1d109b7042dccb4428fda0ae372f42ba794be
Blue Unity Scratch <gomgounity@gmail.com>
2024-04-23 09:56:30 +0200

    Updated Menus to add future patches

--- evilar/Menus/DeletePatchesMenu.sh
+++ evilar/Menus/DeletePatchesMenu.sh
@@ -13,6 +13,10 @@ if [ -d /home/usuario/.config/autostart/Modules/Patches/WallpaperFix/ ]; then ec
 echo ""
 echo ""
 echo ""
+echo ""
+echo ""
+echo ""
+echo ""
 echo " 0) Quit"
 echo ""
 echo ""
@@ -21,7 +25,7 @@ echo "Escribe el Número del Programa a ejecutar (0, 1, 2...):"
 read n
 case $n in
   0) break;;
-  1) if [ -f /home/usuario/.config/autostart/CN_dconf_ibus.desktop ]; then sh -c "$(curl -sSL https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/Uninstaller/CN_ibus/CN_RemovePatch.sh)";fi;;
-  2) if [ -d /home/usuario/.config/autostart/Modules/Patches/WallpaperFix/ ]; then sh -c "$(curl -sSL https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/Uninstaller/XFCE_WP/XFCE_WP_Uninstaller.sh)";fi;;
+  1) if [ -f /home/usuario/.config/autostart/CN_dconf_ibus.desktop ]; then sh -c "$(curl -sSL https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/Uninstaller/CN_ibus/CN_RemovePatch.sh)";else echo "invalid option";fi;;
+  2) if [ -d /home/usuario/.config/autostart/Modules/Patches/WallpaperFix/ ]; then sh -c "$(curl -sSL https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/Uninstaller/XFCE_WP/XFCE_WP_Uninstaller.sh)";else echo "invalid option";fi;;
   *) echo "invalid option";;
 esac
