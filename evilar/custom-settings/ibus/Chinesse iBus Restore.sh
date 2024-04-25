14c3238c1a1cc0b0631df4174c3601ac0cfe6224
Blue Unity Scratch <gomgounity@gmail.com>
2024-04-25 09:46:06 +0200

    Fixed Chinese iBus Patch Installer

--- evilar/custom-settings/ibus/Chinesse iBus Restore.sh
+++ evilar/custom-settings/ibus/Chinesse iBus Restore.sh
@@ -17,11 +17,11 @@ echo "OK"
 cd /home/usuario/.config/autostart/Modules/Patches/CN_Ibus/
 if [ -f /home/usuario/.config/autostart/Modules/Patches/CN_Ibus/CN_Script ]; then rm /home/usuario/.config/autostart/Modules/Patches/CN_Ibus/CN_Script; fi
 echo "OK"
+wget https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/custom-settings/ibus/CN_Script
+echo "OK"
 cd /home/usuario/.config/autostart/
 wget https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/custom-settings/ibus/CN_dconf_ibus.desktop
 cd /home/usuario/.config/autostart/Modules/Patches/CN_Ibus/
-wget https://raw.githubusercontent.com/gomgo-github/GomgoFileChest/Evilar/evilar/custom-settings/ibus/CN_Script
-echo "OK"
 chmod a+x /home/usuario/.config/autostart/Modules/Patches/CN_Ibus/CN_Script
 cd /home/usuario/.config/dconf/
 if [ -f /home/usuario/.config/dconf/CN_dump.dconf ]; then rm /home/usuario/.config/dconf/CN_dump.dconf; fi
